0884fa8e9278683595886df9375f2a2c0f2c8948
nischal2052 <78245209+nischal2052@users.noreply.github.com>
2022-04-20 16:22:39 -0500

    NFT Testing Ayush

---
+++ NFT TESTING AYUSH/nft-drop-starter-project/app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Phantom from './Components/Phantom/Phantom';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+        <Phantom />
+        <Navbar />
+    </>
+  );
+}
+
+export default App;
